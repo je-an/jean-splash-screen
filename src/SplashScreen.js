3f2c9536bb5f94a0e1448538ebb6aa861ac4a5fd
je-an <mail@jeremiasandris.de>
2018-11-01 21:51:47 +0100

    Fix resize bug

--- src/SplashScreen.js
+++ src/SplashScreen.js
@@ -23,7 +23,7 @@ define([ // jscs:ignore
         var SplashScreen = function (options) {
             Inheritance.inheritConstructor(DomElement, this, Merge({ // jscs:ignore
                 path: TypeCheck.isString(options.path) ? options.path : Failure.throwTypeError("options.path is not a string"),
-                html: '<div id="jean-splash-screen"><img src="' + options.path + '" alt="jean-splash-screen"></div>'
+                html: '<div id="jean-splash-screen"></div>'
             }, TypeCheck.isDefined(options) ? options : {}));
             this.element.style.width = "100%";
             this.element.style.height = "100%";
@@ -31,6 +31,13 @@ define([ // jscs:ignore
             this.element.style.zIndex = "10000";
             this.element.style.top = "0";
             this.element.style.left = "0";
+            this.element.style.background = 'url("' + this.options.path + '")';
+            this.element.style.backgroundSize = "cover";
+            this.element.style.backgroundPosition = "center";
+            /*  this.img = DomUtil.getChildById(this.element, "jean-splash-screen-img");
+             this.img.style.width = "100%";
+             this.img.style.height = "100%";
+             this.img.style.backgroundSize = "cover"; */
         };
         Inheritance.inheritPrototype(SplashScreen, DomElement);
         return SplashScreen;
